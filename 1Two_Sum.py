4a35664a49fc550b857d6a3be9213e66572e8af0
Violet-coder <violetzzy6@gmail.com>
2020-12-24 09:58:26 -0500

    validPalindorome2

---
+++ 1Two_Sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target-num] = i
+
+
+solution = Solution()
+outcome = solution.twoSum([2, 7, 11, 15], 9)
+print(outcome)
